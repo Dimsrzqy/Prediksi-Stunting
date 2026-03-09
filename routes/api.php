5468715240a16953d2c85d604d9e678787ac7715
Gilang Pratama Putra <gilangoppo417@gmail.com>
2026-03-09 08:30:49 +0700

    menambah mongo token

--- routes/api.php
+++ routes/api.php
@@ -2,14 +2,25 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AnakController;
+use App\Http\Controllers\PengukuranController;
 
-Route::apiResource('anak', AnakController::class);
+// ==========================================
+// 🔓 AREA PUBLIK (Bebas Masuk Tanpa Token)
+// ==========================================
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
-Route::post('/register', [App\Http\Controllers\AuthController::class, 'register']);
+// ==========================================
+// 🔒 AREA VIP (Wajib Bawa Token Sanctum)
+// ==========================================
+Route::middleware('auth:sanctum')->group(function () {
+    
+    // Semua rute di dalam kotak ini sudah digembok!
+    Route::apiResource('anak', AnakController::class);
+    Route::apiResource('pengukuran', PengukuranController::class);
 
-Route::post('/login', [App\Http\Controllers\AuthController::class, 'login']);
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    // Bonus: Kita buatkan rute untuk Logout (Nanti kita buat fungsinya)
+    // Route::post('/logout', [AuthController::class, 'logout']); 
 });
